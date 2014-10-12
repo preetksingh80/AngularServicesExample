c35de5e1fa3e13a41ce7ddbd6846c1b0c0b61ac0
preet <preet.singh@candorsoftwaresolutions.co.uk>
2014-10-12 23:19:04 +0100

    tested with nested milestone object

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,55 @@
 var app = angular.module('app', []);
 
-app.controller('appController',['$scope', 'service1', 'serviceWay', function($scope, service1, serviceWay){
-    var service = new service1('preet');
-    var service2 = new service1('veeru');
-    serviceWay.setName('service way');
+app.controller('appController',['$scope', 'factory', 'service', 'partiesMilestone', function($scope, factory, service, partiesMilestone){
+    factory.setName('preet');
+    factory.setName('sunny');
+    console.log(factory.getName());
+     console.log(factory.child.getName());
     
+    service.setName('service way 2');
+    //serviceWay.setName('service way 3');
     console.log(service.getName());
-    console.log(service2.getName());
-    console.log(serviceWay);
-    console.log(serviceWay.getName());
-    console.log(serviceWay.child.getName());
+    console.log(service.child.getName());
+    $scope.nameFromFactory = factory.getName() + ',' + factory.child.getName();
+    
+    $scope.nameFromService = service.getName() + ',' + service.child.getName() ;
                                 
-    $scope.name = service.getName();
-                                
-   
+    partiesMilestone.setJob({id: 1});
+                                console.log(partiesMilestone);
+    partiesMilestone.show();
+    console.log(partiesMilestone.url());
+    console.log(partiesMilestone.subMilestones[0].url());
+    
     //console.log('name is ' + $scope.name);
     
     
 }]);
 
-app.factory('service1', function(){
-       
-    var service1 = function(name){
-         this.name = name;
-    };
-    
-    
-    service1.prototype.getName = function(){
-        return this.name;
-    };
-    
+app.factory('factory', function(){
+    console.log('called the factory');
    
+    this.name = '';
+    var self = this;
+    var service1 = {
+        setName: function(name){
+         self.name = name;
+        },
+        getName: function(){ return self.name;},
+        child: {
+            getName: function(){
+                return self.name + ' child';
+                
+            }
+        }
+    }
+    
+    
+      
     return service1;
 });
 
-app.service('serviceWay', function(){
-    
-    console.log('called the service way')   
+app.service('service', function(){
+    console.log('called the service');   
     this.name = '';
     var self = this;
     return{
@@ -46,7 +59,6 @@ app.service('serviceWay', function(){
         getName: function(){ return self.name;},
         child: {
             getName: function(){
-                console.log(self);
                 return self.name + ' child';
                 
             }
@@ -58,3 +70,140 @@ app.service('serviceWay', function(){
     
     
 });
+
+
+app.service("partiesMilestone",  
+    function() {
+        'use strict';
+
+        this.job = {};
+
+        var self = this; // copy the this reference to use in nested object literls
+      
+        return {
+            setJob: function(job) { self.job = job;},
+            name: 'Parties',
+            order: 1,
+            url:  function(){ 
+                console.log(self.job.id);
+                return  '#/parties/' + self.job.id;
+            },
+            show: function () {
+                console.log(self.job);
+                console.log('called parties MileStone show');
+                return true;
+            },
+            icon: 'fa fa-users',
+            path: '#/parties/',
+            subMilestones: [
+                {
+                    name: 'Customer Party',
+                    order: 1,
+                     url:  function(){ 
+                console.log(self.job.id);
+                return  '#/parties/' + self.job.id;
+            },
+                    show: function () {
+                        return true;
+                    },
+                    result: function () {
+                        return self.job.parties[0];
+
+                    }
+                }
+                ,
+                {
+                    name: 'Householder Consent',
+                    order: 1,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return self.job.isGreenDealECO;
+                    },
+                    result: function () {
+                        return self.job.workDocuments.some(function(document) {
+                            if (checkDocumentByType.isDocumentAvailable(self.job, CONST.lookup.documentType.homeOwnerConsent)) {
+                                    return true;
+                                } else return false;
+                            }
+                        );
+                    }
+                }
+                ,
+                {
+                    name: 'Landlord Party',
+                    order: 2,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return $.inArray(job.tenureTypeLookupId, [1, 4]) < 0;
+                    },
+                    result: function () {
+                        if ($.inArray(self.job.tenureTypeLookupId, [1, 4]) >= 0 && self.job.parties[0]) {
+                            return true;
+                        }
+                        else return false;
+
+                    }
+                }
+                ,
+                {
+                    name: 'Landlord Consent',
+                    order: 1,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return $.inArray(self.job.tenureTypeLookupId, [1, 4]) < 0;
+                    },
+                    result: function () {
+                        return self.job.workDocuments.some(function(document) {
+                                if (checkDocumentByType.isDocumentAvailable(self.job, CONST.lookup.documentType.landLordConsent)) {
+                                    return true;
+                                } else return false;
+                            }
+                        );
+                    }
+                }
+                ,
+                {
+                    name: 'ECO Eligibility',
+                    order: 3,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return self.job.isGreenDealECO;
+                    },
+                    result: function () {
+                        return ecoEligibility.isEcoEligible(self.job);
+
+                    }
+                }
+                ,
+                {
+                    name: 'ECO Documentation',
+                    order: 4,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return self.job.isGreenDealECO;
+                    },
+                    result: function () {
+                        return ecoEligibility.isDocumentSupplied(self.job);
+
+                    }
+                }
+                ,
+                {
+                    name: 'ECO Audit',
+                    order: 5,
+                    url: '#/parties/' + self.job.id,
+                    show: function () {
+                        return self.job.isGreenDealECO;
+                    },
+                    result: function () {
+                        return ecoEligibility.isAudited(self.job);
+
+                    }
+                }
+            ],
+            className: 'parties'
+        }
+
+
+    }
+);
